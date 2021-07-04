634175e5a210fbc7fee935e23da8862400cda663
teddy <a21506qoo@gmail.com>
2021-07-04 20:24:21 +0800

    modify: change initial setting with bootstrap.

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
 </template>
 
 <script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 export default {
   name: 'App'
   
--- src/views/start.vue
+++ src/views/start.vue
@@ -1,5 +1,69 @@
 <template>
-  <h1>
-    We start our TWITTER PROJECT here.
-  </h1>
+  <div>
+    <!-- Button trigger modal -->
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-bs-toggle="modal"
+      data-bs-target="#exampleModal"
+    >
+      Launch demo modal
+    </button>
+    <button  
+      class="btn btn-info"
+      @click.prevent.stop="SweetAlertTest"
+    >
+      通知
+    </button>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { Toast } from './../utils/helpers'
+
+export default {
+  name: 'Test',
+  methods: {
+    SweetAlertTest() {
+      Toast.fire({
+        icon: 'success',
+        title: '沒問題啦！'
+      })
+    }
+  }
+}
+</script>
