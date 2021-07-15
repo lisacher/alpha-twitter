7ff228d10cc49b59075ffa7cea14413b964a4b93
teddy <a21506qoo@gmail.com>
2021-07-15 21:52:19 +0800

    Spinner fixed.

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,48 +1,93 @@
 <template>
-  <div class="spinner">
-    <div class="bouncing-loader">
-      <div />
-      <div />
-      <div />
-    </div>
-  </div>
+  <div class="loadingio-spinner-spinner-3998n36ox9f"><div class="ldio-4jdtjhjklh">
+<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+</div></div>
 </template>
 
-<style scoped>
-.spinner {
-  padding-top: 100px;
+<script>
+export default {
+  name: 'Spinner'
 }
+</script>
 
-.bouncing-loader {
-  display: flex;
-  justify-content: center;
+<style scoped type="text/css">
+@keyframes ldio-4jdtjhjklh {
+  0% { opacity: 1 }
+  100% { opacity: 0 }
 }
-
-.bouncing-loader > div {
-  width: 1rem;
-  height: 1rem;
-  margin: 3rem 0.2rem;
-  background: #f60;
-  border-radius: 50%;
-  animation: bouncing-loader 0.6s infinite alternate;
+.ldio-4jdtjhjklh div {
+  left: 47px;
+  top: 24px;
+  position: absolute;
+  animation: ldio-4jdtjhjklh linear 1s infinite;
+  background: #ff6600;
+  width: 6px;
+  height: 12px;
+  border-radius: 2.4px / 2.4px;
+  transform-origin: 3px 26px;
+}.ldio-4jdtjhjklh div:nth-child(1) {
+  transform: rotate(0deg);
+  animation-delay: -0.9166666666666666s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(2) {
+  transform: rotate(30deg);
+  animation-delay: -0.8333333333333334s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(3) {
+  transform: rotate(60deg);
+  animation-delay: -0.75s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(4) {
+  transform: rotate(90deg);
+  animation-delay: -0.6666666666666666s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(5) {
+  transform: rotate(120deg);
+  animation-delay: -0.5833333333333334s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(6) {
+  transform: rotate(150deg);
+  animation-delay: -0.5s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(7) {
+  transform: rotate(180deg);
+  animation-delay: -0.4166666666666667s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(8) {
+  transform: rotate(210deg);
+  animation-delay: -0.3333333333333333s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(9) {
+  transform: rotate(240deg);
+  animation-delay: -0.25s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(10) {
+  transform: rotate(270deg);
+  animation-delay: -0.16666666666666666s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(11) {
+  transform: rotate(300deg);
+  animation-delay: -0.08333333333333333s;
+  background: #ff6600;
+}.ldio-4jdtjhjklh div:nth-child(12) {
+  transform: rotate(330deg);
+  animation-delay: 0s;
+  background: #ff6600;
 }
-
-.bouncing-loader > div:nth-child(2) {
-  animation-delay: 0.2s;
+.loadingio-spinner-spinner-3998n36ox9f {
+  margin: 40%;
+  width: 78px;
+  height: 78px;
+  display: inline-block;
+  overflow: hidden;
+  background: none;
 }
-
-.bouncing-loader > div:nth-child(3) {
-  animation-delay: 0.4s;
-}
-
-@keyframes bouncing-loader {
-  from {
-    opacity: 1;
-    transform: translateY(0);
-  }
-  to {
-    opacity: 0.1;
-    transform: translateY(-1rem);
-  }
+.ldio-4jdtjhjklh {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform: translateZ(0) scale(0.78);
+  backface-visibility: hidden;
+  transform-origin: 0 0; /* see note above */
 }
 </style>
