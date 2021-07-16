f2b74c56dfc4416b3fa46f60627021d4f9f358a1
teddy <a21506qoo@gmail.com>
2021-07-16 17:22:59 +0800

    vue-socket.io setting.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,18 @@ import router from './router'
 import store from './store'
 import './assets/application.css'
 
+// vue-socket.io
+import VueSocketIO from 'vue-socket.io'
+
 Vue.config.productionTip = false
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'socket',
+  vuex: {
+    store
+  }
+}))
 
 new Vue({
   router,
---
+++ src/views/ChatRoom.vue
@@ -0,0 +1,62 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'ChatRoom',
+  data() {
+    return {
+      text: '',
+      onlineUser: [],
+      latestMessage: []
+    }
+  },
+  methods: {
+    async getMessage() {
+      try {
+        // 一進聊天室先請求所有先前聊天訊息
+      const { data } = await api
+        this.latestMessage = {
+          ...this.latestMessage,
+          ...data
+        } 
+      }catch(error) {
+        console.log(error);
+      }
+      
+    },
+    handleSubmit() {
+      // 向socket 發出事件，傳送聊天訊息
+      this.$socket.emit('clientSend', {
+        message: this.text
+      })
+      // 存入事件
+      this.saveMessage()
+      // 清空輸入欄
+      this.text = ''
+    },
+    async saveMessage() {
+      try {
+        // TODO: 請求存入訊息
+
+      } catch(error) {
+        console.log(error);
+      }
+    }
+  },
+  sockets: {
+    connect() {
+      console.log();
+    },
+    otherUserMessage(data) {
+      console.log(`${data.User.name}: ${data.message}`);
+      this.latestMessage.push(data)
+    },
+    myMessage(data) {
+      console.log(`${data.User.name}: ${data.message}`);
+      this.latestMessage.push(data)
+    }
+  }
+}
+</script>
